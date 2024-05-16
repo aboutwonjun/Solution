3d325b4fce9c9f71b1951cd1893a52a5d9942b84
aboutwonjun <wjdnjswns@gmail.com>
2024-05-16 15:54:34 +0900

    .

--- Solution/0516 class/Practice 1.cpp
+++ Solution/0516 class/Practice 1.cpp
@@ -1,3 +1,16 @@
+//실습 1 상속
+// 1) Shpae 라는 클래스 생성
+// 1-1 변의 개수, 밑변의 길이를 저장하는 변수 가지고 있어야 함, 그리고 이를 출력하는 printinfo 함수 있어야함
+
+//2) Shape 클래스 상속 받는, Rectangle, Triangle 생성
+//2-1 Rectangle에는 세로 길이를 의미하는 변수 
+//2-2 Triangle에는 높이 길이를 의미하는 변수 
+//2-3 각각 도형의 넓이를 구하는 area 함수 
+//2-4 두 클래스 모두 생성자에서 모든 변수에 값을 대입해야 함
+
+
+
+
 #include <iostream>
 
 using namespace std;
@@ -7,16 +20,17 @@ class Shape
 {
 public:
 
+	int numSide;
+	int lengthBase;
+
 	Shape() //생성자호출 
 	{
 		numSide = 0;
 		lengthBase = 0;
 	}
 
-	int numSide;
-	int lengthBase;
 
-	void printfInfo(int numSide, int lengthBase)
+	void printfInfo()
 	{
 		cout << "변의 개수" << numSide;
 		cout << "밑변의 길이" << lengthBase;
@@ -32,8 +46,10 @@ public:
 	//생성자에서 모든 변수에 값을 대입해야함 
 	Rectangle(int length_base, int verticalLength)
 	{
+
 		m_verticalLength = verticalLength;
 		lengthBase = length_base;
+		numSide = 4;
 	}
 
 	void area()
@@ -55,7 +71,8 @@ public:
 	Triangle(int height, int length_base)
 	{
 		m_height = height;
-		this->lengthBase = lengthBase;
+		lengthBase = lengthBase;
+		numSide = 3;
 	}
 
 	void area()
@@ -70,10 +87,12 @@ int main()
 {
 	// 사각형 인스턴스 생성 
 	Rectangle rectangle(5, 10); // Width: 5, Length: 10
+	rectangle.printfInfo();
 	rectangle.area(); 
 
 	// 삼각형 인스턴스 생성 
 	Triangle triangle(4, 6);
+	triangle.printfInfo();
 	triangle.area();
 
 	return 0;
