5d378d983e870a34a3c7239d43f55ee34c550f11
aboutwonjun <wjdnjswns@gmail.com>
2024-05-16 20:27:33 +0900

    Practice 5

--- Solution/0516 class/Practice5.cpp
+++ Solution/0516 class/Practice5.cpp
@@ -8,9 +8,12 @@
 * 7. 반복문을 통해 간식들의 상품 이름을 출력 
 */
 
+#include <iostream>
+#include <string>
+using namespace std;
 
 
-
+// 1. 모체 Snack Class 생성 / 가격, 상품 이름, 제조회사 (공통) 
 #include <iostream>
 #include <string>
 using namespace std;
@@ -19,70 +22,124 @@ using namespace std;
 // 1. 모체 Snack Class 생성 / 가격, 상품 이름, 제조회사 (공통) 
 class Snack
 {
+
+private:
+	int m_price;
+	string m_productName;
+	string m_company;
+
 public:
-	int price;
-	string productName;
-	string company;
+	// Constructor
+	Snack() {}
+
+	// Getter 및 Setter
+	int getPrice()
+	{
+		return m_price;
+	}
+	void setPrice(int price)
+	{
+		m_price = price;
+	}
+
+	string getProductName()
+	{
+		return m_productName;
+	}
+	void setProductName(string productName)
+	{
+		m_productName = productName;
+	}
+
+	string getCompany()
+	{
+		return m_company;
+	}
+	void setCompany(string company)
+	{
+		m_company = company;
+	}
 };
 
 // 2. Candy 생성 / 맛 추가, 나머진 상속 
 class Candy : public Snack
 {
-public : 
-	string taste;
-};
+private:
+	string m_taste;
 
+public:
+	// Constructor
+	Candy() {}
+
+	// Getter 및 Setter
+	string getTaste()
+	{
+		return m_taste;
+	}
+	void setTaste(string taste)
+	{
+		m_taste = taste;
+	}
+};
 
 // 3. Choco 생성 / 모양추가, 나머진 상속 
 class Chocolate : public Snack
 {
+private:
+	string m_shape;
+
 public:
-	string shape;
+	// Constructor
+	Chocolate() {}
+
+	// Getter 및 Setter
+	string getShape()
+	{
+		return m_shape;
+	}
+	void setShape(string shape)
+	{
+		m_shape = shape;
+	}
 };
 
 
 int main()
 {
 	Candy candy1;
-	candy1.taste = "sweet";
-	candy1.price = 1000;
-	candy1.productName = "HI";
-	candy1.company = "A";
-
+	candy1.setTaste("sweet");
+	candy1.setPrice(1000);
+	candy1.setProductName("HI");
+	candy1.setCompany("A");
 
 	Candy candy2;
-	candy2.taste = "Sour";
-	candy2.price = 1200;
-	candy2.productName = "Hello";
-	candy2.company = "B";
+	candy2.setTaste("Sour");
+	candy2.setPrice(1200);
+	candy2.setProductName("Hello");
+	candy2.setCompany("B");
 
 	Chocolate choco1;
-	choco1.shape = "HeartShape";
-	choco1.price = 1200;
-	choco1.productName = "Love";
-	choco1.company = "C";
+	choco1.setShape("HeartShape");
+	choco1.setPrice(1200);
+	choco1.setProductName("Love");
+	choco1.setCompany("C");
 
 	Chocolate choco2;
-	choco2.shape = "StarShape";
-	choco2.price = 1200;
-	choco2.productName = "StaryNight";
-	choco2.company = "D";
+	choco2.setShape("StarShape");
+	choco2.setPrice(1200);
+	choco2.setProductName("StaryNight");
+	choco2.setCompany("D");
 
 	//2-2 4객체를 SnackBasket에 넣기
+	Snack snackBasket[4] = { candy1, candy2, choco1, choco2 };
 
-	//객체 배열! https://blog.hexabrain.net/174
-	Snack snackBasket[4] = {candy1, candy2, choco1, choco2 };
-
-
-	for(int i = 0; i < 4; i++)
+	for (int i = 0; i < 4; i++)
 	{
-		cout << i << "번째 상품 이름 : "<<  snackBasket[i].productName << endl;
+		cout << i << "번째 상품 이름 : " << snackBasket[i].getProductName() << endl;
 	}
 
 	return 0;
-
-	}
-
+}
 
 
 
